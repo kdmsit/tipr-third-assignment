0ba58b8033d2642c4ffa1f386014f4863b8daae6
KISHALAY DAS <kishalay.msit@gmail.com>
2019-03-17 15:27:23 +0530

    17th March Commit

--- src/main.py
+++ src/main.py
@@ -206,6 +206,7 @@ if __name__ == '__main__':
                 # Calculate the accuracy on the batch of training data
                 #train_accuracy += sess.run(accuracy, feed_dict=feed_dict_train)
                 acc,y_pred_cls = sess.run([accuracy,y_pred],feed_dict=feed_dict_train)
+                print(y_pred)
                 train_accuracy +=acc
                 y_pred_label.append(y_pred_cls)
 
