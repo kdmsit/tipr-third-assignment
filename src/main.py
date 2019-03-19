cc4efb3d004799699d1e725b0ae6cab532ebae57
KISHALAY DAS <kishalay.msit@gmail.com>
2019-03-19 15:38:10 +0530

    19/03 commit

--- src/main.py
+++ src/main.py
@@ -89,10 +89,10 @@ if __name__ == '__main__':
         trainfilepath = sys.argv[2]
         testfilepath = sys.argv[4]
         datasetname = sys.argv[6]
-        # con=sys.argv[8].split('')
         con = sys.argv[8]
         x = []
         con = con[1:-1]
+        print(con)
         con = con.split(',')
         x = []
         for k in con:
